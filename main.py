2d75f2165effc72a811b0466b1aa2dd328342b38
Kamila Krasniewska <kamila.krasniewska@o2.pl>
2024-05-22 10:52:45 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+#Funkcje do napisania:
+#Obliczanie ceny brutto na podstawie ceny netto - WERONIKA
+#Dodawanie pacjentów - Andrzej
+#Dodawanie informacji o wizycie (kiedy, kto, co, jakie leki zapisane + cena za zabieg powiazania z obliczeniem ceny brutto) - Lena
