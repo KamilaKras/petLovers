5723e8f0872ed1ddb2f60c2558eb621ccc0a7243
Kamila Krasniewska <kamila.krasniewska@o2.pl>
2024-06-03 13:59:51 +0200

    corrcted issue with json endocing

--- data.py
+++ data.py
@@ -2,13 +2,15 @@ import json
 import os
 
 def wczytaj_dane(plik):
-    if not os.path.exists(plik):
-        return []
-    with open(plik, 'r') as file:
-        try:
+    try:
+        with open(plik, 'r', encoding='utf-8') as file:
             return json.load(file)
-        except json.JSONDecodeError:
-            return []
+    except json.JSONDecodeError:
+        print("Błąd dekodowania pliku JSON.")
+        return []
+    except UnicodeDecodeError:
+        print("Błąd dekodowania pliku. Upewnij się, że plik jest zapisany w kodowaniu UTF-8.")
+        return []
 
 def zapisz_dane(plik, dane):
     with open(plik, 'w') as file:
